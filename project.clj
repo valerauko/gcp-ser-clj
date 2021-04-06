2bcf79ab791b3099d7808f858c699214256e4f32
Vale <valerauko@gmail.com>
2021-04-06 18:25:08 +0900

    Release 0.7.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gcp-ser-clj "0.1.0-SNAPSHOT"
+(defproject gcp-ser-clj "0.7.0"
   :description "A Clojure library trying to make reporting errors to Google Cloud's Error Reporting system (Stackdriver) simpler."
   :url "https://github.com/valerauko/gcp-ser-clj"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
