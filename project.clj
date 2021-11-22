869ef211abf7b70ea6dc1e09800a54f1aa64c7c9
Vale <valerauko@gmail.com>
2021-11-22 13:45:27 +0900

    Bump to latest google package

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/tools.logging "1.1.0"]
-                 [com.google.cloud/google-cloud-errorreporting "0.122.5-beta"]]
+                 [com.google.cloud/google-cloud-errorreporting "0.122.7-beta"]]
   :repl-options {:init-ns gcp-ser-clj.core}
   :deploy-repositories [["github" "https://maven.pkg.github.com/valerauko/gcp-ser-clj"]]
   :profiles {:dev {:dependencies
