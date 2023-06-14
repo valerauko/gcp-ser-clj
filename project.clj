49d4481da28981feb1655cff4b1a8218736bb2c3
ERDOS Balint <valerauko@gmail.com>
2023-06-14 13:05:29 +0900

    Add scm info to project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,7 @@
 (defproject gcp-ser-clj "0.8.1"
+  :scm {:name "git"
+        :url "https://github.com/valerauko/gcp-ser-clj"
+        :tag "v0.8.1"}
   :description "A Clojure library trying to make reporting errors to Google Cloud's Error Reporting system (Stackdriver) simpler."
   :url "https://github.com/valerauko/gcp-ser-clj"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
