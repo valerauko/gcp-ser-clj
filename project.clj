61ed40207bb2ad5091336a086e6284e02c4d52b1
Vale <valerauko@gmail.com>
2023-10-04 15:52:08 +0900

    Update gcp deps too

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/tools.logging "1.2.4"]
-                 [com.google.cloud/google-cloud-errorreporting "0.124.12-beta"]]
+                 [com.google.cloud/google-cloud-errorreporting "0.148.0-beta"]]
   :repl-options {:init-ns gcp-ser-clj.core}
   :deploy-repositories {"github" {:url "https://maven.pkg.github.com/valerauko/gcp-ser-clj"
                                   :username :env/github_user
